cabc37e02bb9e4bb6bd06543d478baea88854541
ron smith <ron.smith37@gmail.com>
2025-06-27 18:56:45 +1000

    removed debug comments

--- users/auth_backends.py
+++ users/auth_backends.py
@@ -3,6 +3,7 @@ from django.conf import settings
 from django.contrib.auth.backends import BaseBackend
 from users.models import RemoteUser
 
+
 class RemoteAuthBackend(BaseBackend):
     """
     Authenticate against the external auth server.
@@ -11,7 +12,6 @@ class RemoteAuthBackend(BaseBackend):
     def authenticate(self, request, username=None, password=None):
         # POST to your auth-server’s login endpoint:
         try:
-            print(f"Authenticating user {username} with password {password}")
             url = settings.AUTH_SERVER_URL.rstrip('/') + '/users/login/'
             resp = requests.post(
                 url,
@@ -20,17 +20,15 @@ class RemoteAuthBackend(BaseBackend):
             )
         except requests.RequestException:
             return None
-        
+
         data = resp.json()
-        print('Response from auth server:', data)
         if resp.status_code == 200:
-            token = data.get('tokens', {}).get('access') 
+            token = data.get('tokens', {}).get('access')
             profile = data.get('user')
-            print('profile', profile)
 
             user, created = RemoteUser.objects.get_or_create(
                 id=profile['id'],
-                defaults = {
+                defaults={
                     'first_name': profile['first_name'],
                     'last_name': profile['last_name'],
                     'initials': profile['initials'],
